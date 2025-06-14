0bb4378f5397c862cad84e67e15384519d05279d
Bardan karki <152900073+bardan8586@users.noreply.github.com>
2025-06-14 12:34:40 +1000

    first commit

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import streamlit as st
+import pandas as pd
+st.set_page_config(page_title="Amazon Data Viewer", layout="wide")
+file = pd.read_csv('./data/amazon.csv')
 
 st.write("Hello, Streamlit!")
+st.write(file)
